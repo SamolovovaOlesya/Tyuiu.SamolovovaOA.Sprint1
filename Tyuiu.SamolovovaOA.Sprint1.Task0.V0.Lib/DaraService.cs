81975833915e4138a90325aec02e3c1a309371df
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-09-18 22:48:11 +0500

    Add project files.

---
+++ Tyuiu.SamolovovaOA.Sprint1.Task0.V0.Lib/DaraService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SamolovovaOA.Sprint1.Task0.V0.Lib
+{
+    public class DaraService : ISprint1Task0V0
+    {
+
+        public double Calculate()
+        {
+            return 10 / (2 + 3);
+        }
+    }
+}
+
---
+++ Tyuiu.SamolovovaOA.Sprint1.Task0.V0.Test/DaraServiceTest.cs
@@ -0,0 +1,17 @@
+﻿
+using Tyuiu.SamolovovaOA.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public sealed class DaraServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DaraService ds = new DaraService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
