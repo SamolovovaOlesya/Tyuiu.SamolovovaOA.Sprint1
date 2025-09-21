a7db8ec0c7d88d21d24ff7c4d39e90c1e9f3d37c
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-09-22 00:33:27 +0500

    Task0

---
+++ Tyuiu.SamolovovaOA.Sprint1.Task0.V26.Lib/DaraService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SamolovovaOA.Sprint1.Task0.V26.Lib
+{
+    public class DaraService : ISprint1Task0V26
+    {
+        public double Calculate()
+        {
+            return 4 * 5 / 2 - 18 / 2 / 3;
+        }
+
+    }
+}
---
+++ Tyuiu.SamolovovaOA.Sprint1.Task0.V26.Test/DaraServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SamolovovaOA.Sprint1.Task0.V26.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint1.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DaraServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DaraService ds = new DaraService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
