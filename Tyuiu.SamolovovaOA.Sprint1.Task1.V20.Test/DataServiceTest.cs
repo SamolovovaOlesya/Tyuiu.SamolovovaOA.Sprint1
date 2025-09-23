3e5c7097372e77342f6ea5468ae5e4c1751761b7
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-09-23 17:41:24 +0500

    Task1

---
+++ Tyuiu.SamolovovaOA.Sprint1.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.SamolovovaOA.Sprint1.Task1.V20.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint1.Task1.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3.0;
+            double y = 2.0;
+            
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(13, res);
+        }
+    }
+}
---
+++ Tyuiu.SamolovovaOA.Sprint1.Task1.V20/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.SamolovovaOA.Sprint1.Task1.V20.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Самоловова О. А. | ПИНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в S#                                        *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #20                                                             *");
+        Console.WriteLine("* Выполнила: Самоловова Олеся Алексеевна | ПИНб-25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле (x*y/2) + 10 и печатает его на экране.   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+
+        Console.WriteLine("Введите значение X: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение Y: ");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x, y));
+
+        Console.ReadLine();
+    }
+}
