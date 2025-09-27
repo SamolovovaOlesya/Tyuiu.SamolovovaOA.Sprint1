df3ac40ca31c18cddb638e8de98b3d39ffceaa89
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-09-27 14:57:16 +0500

    Task4

---
+++ Tyuiu.SamolovovaOA.Sprint1.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SamolovovaOA.Sprint1.Task4.V3.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint1.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double x = 3;
+            double y = 2;
+            double wait = 3;
+
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SamolovovaOA.Sprint1.Task4.V3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.SamolovovaOA.Sprint1.Task4.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+
+        Console.WriteLine("Введите значение X: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение Y: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("√3x/(x - y)^2 = " + ds.Calculate(x, y));
+        
+        Console.ReadLine();
+    }
+}
